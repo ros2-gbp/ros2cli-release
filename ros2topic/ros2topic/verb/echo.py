4a9a47db32011f31f5e8b46a5706d5ad763ae90d
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-11-02 19:15:59 +0000

    Rebase from 'upstream'

--- ros2topic/ros2topic/verb/echo.py
+++ ros2topic/ros2topic/verb/echo.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from argparse import ArgumentTypeError
 from typing import Any
 from typing import Callable
 from typing import Optional
@@ -21,11 +20,16 @@ from typing import TypeVar
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
+from rclpy.qos_event import SubscriptionEventCallbacks
+from rclpy.qos_event import UnsupportedEventTypeError
+from rclpy.utilities import get_rmw_implementation_identifier
+from ros2cli.node.strategy import add_arguments as add_strategy_node_arguments
 from ros2cli.node.strategy import NodeStrategy
 from ros2topic.api import add_qos_arguments_to_argument_parser
 from ros2topic.api import get_msg_class
 from ros2topic.api import qos_profile_from_short_keys
 from ros2topic.api import TopicNameCompleter
+from ros2topic.api import unsigned_int
 from ros2topic.verb import VerbExtension
 from rosidl_runtime_py import message_to_csv
 from rosidl_runtime_py import message_to_yaml
@@ -35,20 +39,12 @@ DEFAULT_TRUNCATE_LENGTH = 128
 MsgType = TypeVar('MsgType')
 
 
-def unsigned_int(string):
-    try:
-        value = int(string)
-    except ValueError:
-        value = -1
-    if value < 0:
-        raise ArgumentTypeError('value must be non-negative integer')
-    return value
-
-
 class EchoVerb(VerbExtension):
     """Output messages from a topic."""
 
     def add_arguments(self, parser, cli_name):
+        add_strategy_node_arguments(parser)
+
         arg = parser.add_argument(
             'topic_name',
             help="Name of the ROS topic to listen to (e.g. '/chatter')")
@@ -76,6 +72,10 @@ class EchoVerb(VerbExtension):
             '--no-arr', action='store_true', help="Don't print array fields of messages")
         parser.add_argument(
             '--no-str', action='store_true', help="Don't print string fields of messages")
+        parser.add_argument(
+            '--lost-messages', action='store_true', help='Report when a message is lost')
+        parser.add_argument(
+            '--raw', action='store_true', help='Echo the raw binary representation')
 
     def main(self, *, args):
         return main(args)
@@ -89,18 +89,30 @@ def main(args):
         truncate_length = args.truncate_length if not args.full_length else None
         callback = subscriber_cb_csv(truncate_length, args.no_arr, args.no_str)
     qos_profile = qos_profile_from_short_keys(
-        args.qos_profile, reliability=args.qos_reliability, durability=args.qos_durability)
+        args.qos_profile, reliability=args.qos_reliability, durability=args.qos_durability,
+        depth=args.qos_depth, history=args.qos_history)
     with NodeStrategy(args) as node:
         if args.message_type is None:
-            message_type = get_msg_class(node, args.topic_name, include_hidden_topics=True)
+            message_type = get_msg_class(
+                node, args.topic_name, include_hidden_topics=True)
         else:
-            message_type = get_message(args.message_type)
+            try:
+                message_type = get_message(args.message_type)
+            except (AttributeError, ModuleNotFoundError, ValueError):
+                raise RuntimeError('The passed message type is invalid')
 
         if message_type is None:
-            raise RuntimeError('Could not determine the type for the passed topic')
+            raise RuntimeError(
+                'Could not determine the type for the passed topic')
 
         subscriber(
-            node, args.topic_name, message_type, callback, qos_profile)
+            node,
+            args.topic_name,
+            message_type,
+            callback,
+            qos_profile,
+            args.lost_messages,
+            args.raw)
 
 
 def subscriber(
@@ -108,27 +120,59 @@ def subscriber(
     topic_name: str,
     message_type: MsgType,
     callback: Callable[[MsgType], Any],
-    qos_profile: QoSProfile
+    qos_profile: QoSProfile,
+    report_lost_messages: bool,
+    raw: bool
 ) -> Optional[str]:
     """Initialize a node with a single subscription and spin."""
-    node.create_subscription(
-        message_type, topic_name, callback, qos_profile)
-
+    event_callbacks = None
+    if report_lost_messages:
+        event_callbacks = SubscriptionEventCallbacks(
+            message_lost=message_lost_event_callback)
+    try:
+        node.create_subscription(
+            message_type,
+            topic_name,
+            callback,
+            qos_profile,
+            event_callbacks=event_callbacks,
+            raw=raw)
+    except UnsupportedEventTypeError:
+        assert report_lost_messages
+        print(
+            f"The rmw implementation '{get_rmw_implementation_identifier()}'"
+            ' does not support reporting lost messages'
+        )
     rclpy.spin(node)
 
 
 def subscriber_cb(truncate_length, noarr, nostr):
     def cb(msg):
         nonlocal truncate_length, noarr, nostr
-        print(
-            message_to_yaml(
-                msg, truncate_length=truncate_length, no_arr=noarr, no_str=nostr),
-            end='---\n')
+        if isinstance(msg, bytes):
+            print(msg, end='\n---\n')
+        else:
+            print(
+                message_to_yaml(
+                    msg, truncate_length=truncate_length, no_arr=noarr, no_str=nostr),
+                end='---\n')
     return cb
 
 
 def subscriber_cb_csv(truncate_length, noarr, nostr):
     def cb(msg):
         nonlocal truncate_length, noarr, nostr
-        print(message_to_csv(msg, truncate_length=truncate_length, no_arr=noarr, no_str=nostr))
+        if isinstance(msg, bytes):
+            print(msg)
+        else:
+            print(message_to_csv(msg, truncate_length=truncate_length, no_arr=noarr, no_str=nostr))
     return cb
+
+
+def message_lost_event_callback(message_lost_status):
+    print(
+        'A message was lost!!!\n\ttotal count change:'
+        f'{message_lost_status.total_count_change}'
+        f'\n\ttotal count: {message_lost_status.total_count}',
+        end='---\n'
+    )
