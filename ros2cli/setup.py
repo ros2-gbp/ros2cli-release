dacfeb1fd5b341f1ee841368705a461cabe188b7
Chris Lalancette <clalancette@gmail.com>
2024-02-07 13:38:43 +0000

    Imported upstream version '0.31.1' of 'upstream'

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.25.5',
+    version='0.31.1',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
