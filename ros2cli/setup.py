105683195687f18ddfcb2644df7a27fa0ae16156
Yadunund <yadunund@openrobotics.org>
2024-04-19 21:06:53 +0800

    Imported upstream version '0.25.6' of 'upstream'

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.32.0',
+    version='0.25.6',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
