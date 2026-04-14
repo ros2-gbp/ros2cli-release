7fd2057fd9dc2fc38b645635bf4c020f5f00cb95
Shane Loretz <sloretz@intrinsic.ai>
2026-04-14 05:24:12 +0000

    Rebase from 'upstream'

--- ros2action/ros2action/verb/send_goal.py
+++ ros2action/ros2action/verb/send_goal.py
@@ -68,8 +68,10 @@ class SendGoalVerb(VerbExtension):
         parser.add_argument(
             '-t', '--timeout', metavar='N', type=non_negative_int, default=None,
             help=(
-                'Wait for N seconds until server becomes available and goal is completed '
-                '(default waits indefinitely)'
+                'Wait for N seconds until action server becomes available, '
+                'goal is sent, and result is received. '
+                'Also applies to goal cancellation if interrupted. '
+                '(default: waits indefinitely)'
             ))
 
     def main(self, *, args):
@@ -145,20 +147,27 @@ def send_goal(action_name, action_type, goal_values, feedback_callback, timeout=
 
         print('Sending goal:\n     {}'.format(message_to_yaml(goal)))
         goal_future = action_client.send_goal_async(goal, feedback_callback)
-        rclpy.spin_until_future_complete(node, goal_future)
+        rclpy.spin_until_future_complete(node, goal_future, timeout_sec=timeout)
+
+        if not goal_future.done():
+            print(f'Timed out waiting for goal acceptance after {timeout} seconds.')
+            return
 
         goal_handle = goal_future.result()
 
         # install signal handler to cancel the goal on SIGINT
         def _sigint_cancel_handler(sig, frame):
-            nonlocal goal_handle
             # Cancel the goal if it's still active
             if (goal_handle is not None and
                 (GoalStatus.STATUS_ACCEPTED == goal_handle.status or
                  GoalStatus.STATUS_EXECUTING == goal_handle.status)):
                 print('Canceling goal...')
                 cancel_future = goal_handle.cancel_goal_async()
-                rclpy.spin_until_future_complete(node, cancel_future)
+                rclpy.spin_until_future_complete(node, cancel_future, timeout_sec=timeout)
+
+                if not cancel_future.done():
+                    raise RuntimeError(
+                        f'Timed out waiting for goal cancellation after {timeout} seconds.')
 
                 cancel_response = cancel_future.result()
 
